41627db087a28bac9f5a8b0d0905cfd30167ce08
liqiang <923883928@qq.com>
2018-10-24 16:14:28 +0800

    创建kafk项目

---
+++ src/main/java/et/liq/st/kafka/KafkStApplication.java
@@ -0,0 +1,12 @@
+package et.liq.st.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkStApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkStApplication.class, args);
+	}
+}
